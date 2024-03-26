bba1fc58ee6a696261e8c9e262f71ce25e697775
urim <jiurim0316@naver.com>
2024-03-26 22:13:27 +0900

    Feat : Initial settings

---
+++ src/test/java/nego_github_action/kimjinoh/KimjinohApplicationTests.java
@@ -0,0 +1,13 @@
+package nego_github_action.kimjinoh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KimjinohApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
