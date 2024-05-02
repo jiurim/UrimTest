d93f04c5931ee3687696ef1b18e27fff18189e4c
urim <jiurim0316@naver.com>
2024-05-03 01:39:30 +0900

    Feat : DB 연결

--- src/main/java/nego_github_action/kimjinoh/manager/controller/TestController.java
+++ src/main/java/nego_github_action/kimjinoh/manager/controller/TestController.java
@@ -1,4 +1,4 @@
-package nego_github_action.kimjinoh.test;
+package nego_github_action.kimjinoh.manager.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,12 +7,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping(value = "/")
 	public String doGetHelloWorld() {
-		return "진짜 개 그지같네 드디어";
+		return "테스트 입니다.";
 	}
 
     @GetMapping(value = "/nice")
 	public String doGetHelloWorldd() {
-		return "고양이가 걸어다닙니다???????";
+		return "꽁꽁 얼어붙은 한강위로 고양이가 걸어다닙니다.";
 	}
 
 
