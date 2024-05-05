df0eaf1e93763132a3e83d3f26433789db2a16d8
urim <jiurim0316@naver.com>
2024-05-06 01:15:12 +0900

    Fix : Deploy.sh 3

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,10 +4,9 @@ REPOSITORY=/home/ubuntu/app
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
+CURRENT_PID="$./kimjinoh-0.0.1-SNAPSHOT.jar"
 CURRENT_PID=$(pgrep -fla java | grep kimjinoh | awk '{print $1}')
 
-
-#CURRENT_PID="$REPOSITORY/kimjinoh-0.0.1-SNAPSHOT.jar"
 #CURRENT_PID=$(pgrep -fla java | grep hayan | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
